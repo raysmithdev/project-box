13bd637ab68cf68095141239878394ed370c1658
kfuquay <kirsty.fuquay@gmail.com>
2019-05-19 13:55:37 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+// import ProjectBoxContext from './ProjectBoxContext';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [],
+    };
+  }
+
+  render() {
+    // const contextValue = {
+    //   projects: this.state.projects,
+    // };
+
+    return (
+      <div className="App">
+      
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/ProjectBoxContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProjectBoxContext = React.createContext({
+    projects: [],
+});
+
+export default ProjectBoxContext;
