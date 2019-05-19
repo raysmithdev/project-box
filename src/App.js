38a94ebdc94b752194ae2da410f083a7bad016c7
kfuquay <kirsty.fuquay@gmail.com>
2019-05-19 15:55:33 -0400

    add login form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Nav from "./components/Navigation/Nav";
 import NotFound from "./components/NotFound/NotFound";
 import ProjectList from "./components/ProjectList/ProjectList";
+import Login from "./components/Login/Login";
 import "./App.css";
 
 class App extends Component {
@@ -22,6 +23,7 @@ class App extends Component {
         <main className="app-main">
           <Switch>
             <Route exact path={"/"} component={ProjectList} />
+            <Route path={"/login"} component={Login} />
             <Route component={NotFound} />
           </Switch>
         </main>
