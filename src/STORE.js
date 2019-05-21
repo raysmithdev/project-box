856b15757f41834302a89580f2269bfcc882286b
kfuquay <kirsty.fuquay@gmail.com>
2019-05-21 08:04:44 -0400

    add store.js

---
+++ src/STORE.js
@@ -0,0 +1,13 @@
+export default {
+    projects: [
+        {
+            username: 'knitpearl77',
+            id: 1,
+            title: 'Knit Sweater',
+            summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            materials: '1200 yards worsed weight yar.  size 6 circular needles. 4 x size 6 double pointed needles.  stitch markers.  1 x yarn needle',
+            steps: 'cast on. knit bottom-up.  knit sweater.  bind off. wear.',
+
+        }
+    ]
+}
